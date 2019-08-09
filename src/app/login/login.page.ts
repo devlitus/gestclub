9d2e0bc01a631276c29af3fc1c326a98ffed9972
devlitus <developercarles@gmail.com>
2019-08-09 11:59:58 +0200

    verificar input login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { isNullOrUndefined } from 'util';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertCtrl: AlertController) { }
 
   ngOnInit() {
+  }
+  onSubmit(form: NgForm){
+    if(isNullOrUndefined(form.value.name) || isNullOrUndefined(form.value.pass)){
+      this.alert();
+    }else if (form.value.name === "" || form.value.pass === ""){
+      this.alert();
+    }
+  }
+
+  async alert (){
+    let alert = await this.alertCtrl.create({
+      header: 'Usuario Incorrecto!',
+      subHeader: 'Hable con el administrador o intente de nuevo',
+      buttons: ["Aceptar"]
+    })
+    await alert.present()
   }
 
 }
