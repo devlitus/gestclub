d45fef99c5daa811f55375c591d82bbdb624ceb6
devlitus <developercarles@gmail.com>
2019-09-03 11:33:25 +0200

    modifiacr config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,4 +2,7 @@
 // export const url: string = 'http://tecnovila.es/apiGestDepor/v1';
 
 // Desarrollo
-export const url: string = 'http://localhost/apigestdepor';
+// export const url: string = 'http://localhost/apigestdepor';
+
+// Desarrolo linux
+export const url: string = 'http://myserver.net/apigestdepor';
